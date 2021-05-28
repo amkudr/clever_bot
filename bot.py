ec3817d46255668e395de7ce7bd0ef501535cc37
Anton Kudryashov <amkudr@mail.ru>
2021-05-28 20:04:12 +0300

    Бот научился играть в города

---
+++ bot.py
@@ -0,0 +1,50 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import settings
+import cities
+
+logging.basicConfig(filename="bot.log", level=logging.INFO)
+def greet_user(update,context):
+    print("Пользователь найден")
+    update.message.reply_text('Олды тут?')
+
+    print(update)
+def talk_to_me(update, context):
+    text = update.message.text
+    print(text)
+    update.message.reply_text(text)
+
+def city_game(update, context):
+    print("Игра началась")
+    game_list = cities.CITIES   
+    text = update.message.text.split()
+    if len(text) == 1:
+        update.message.reply_text("Введите город") 
+    user_city = text[1].lower().capitalize()
+ 
+    if game_list.count(user_city) == 1:
+        game_list.remove(user_city)
+        letter = user_city[-1].upper()
+        for city in game_list:
+            if city[0] == letter:
+                bot_city = city
+                update.message.reply_text(f'{bot_city} , ваш ход')
+                break
+
+
+                  
+    else:
+        update.message.reply_text('Нет такого города')  
+
+
+def main():
+    mybot = Updater(settings.API_KEY, use_context=True)
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("cities", city_game))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    logging.info("start botik")
+    mybot.start_polling()
+    mybot.idle()
+if __name__ == "__main__":
+    main()
